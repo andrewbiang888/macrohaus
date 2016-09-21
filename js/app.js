fe3fe59a1105e44df30e11935acbf2ff0852940e
Andrew Erickson <andrew@macroconscious.com>
2016-09-20 20:57:13 -0700

    added json file

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ particlesJS.load('particles-js', 'particles.json', function() {
 */
 
 /* Otherwise just put the config content (json): */
-
+/* ---- particles.js config ---- */
 particlesJS('particles-js',
 
   {
